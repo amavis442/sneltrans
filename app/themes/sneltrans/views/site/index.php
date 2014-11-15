1b52d998e45dd617a4ee9ec466ce04fd41115e08
Patrick Teunissen <p.teunissen18@gmail.com>
2014-11-16 00:19:43 +0100

    Added banner and translation

--- app/themes/sneltrans/views/site/index.php
+++ app/themes/sneltrans/views/site/index.php
@@ -1,25 +1,24 @@
 <?php
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+use yii\captcha\Captcha;
+
 /* @var $this yii\web\View */
 $this->title = 'Sneltrans';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                
-                <img alt="" src="<?=Yii::getAlias('@web');?>/images/transportation-wordpress-theme-320x107.jpg">
-                
-                
+
+                <img alt="" src="<?= Yii::getAlias('@web'); ?>/images/transportation-wordpress-theme-320x107.jpg">
+
+
                 <h2>Heading</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
@@ -27,7 +26,7 @@ $this->title = 'Sneltrans';
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/"><?=Yii::t('app','Read more');?> &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <img alt="" src="images/packing-moving-theme-320x107.jpg">
@@ -38,7 +37,7 @@ $this->title = 'Sneltrans';
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/"><?=Yii::t('app','Read more');?> &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <img alt="" src="images/wordpress-moving-company-theme-320x107.jpg">
@@ -49,9 +48,39 @@ $this->title = 'Sneltrans';
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/"><?=Yii::t('app','Read more');?> &raquo;</a></p>
             </div>
         </div>
 
     </div>
 </div>
+
+<div class='container-fluid scheidingbalk'>
+        tussen balk
+</div>
+
+<div class="site-contact">
+    <h1>Pakket opgeven</h1>
+    <div class="row">
+        <div class="col-lg-5">
+            <?php
+            $form = ActiveForm::begin(['id' => 'contact-form']);
+            ?>
+            <?= $form->field($model, 'name') ?>
+            <?= $form->field($model, 'email') ?>
+            <?= $form->field($model, 'subject') ?>
+            <?= $form->field($model, 'body')->textArea(['rows' => 6]) ?>
+            <?=
+            $form->field($model, 'verifyCode')->widget(Captcha::className(), [
+                'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
+            ])
+            ?>
+            <div class="form-group">
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+            </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+</div>
+
+
